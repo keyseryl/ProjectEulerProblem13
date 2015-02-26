0d530e2f726b328ad62ddab4d862ed6f80651d09
keyseryl <zhenyalkeyser@gmail.com>
2015-02-26 06:08:55 -0500

    "Added coments"

---
+++ src/edu/uc/IT2045C/Spring2015/Assignment07/keyseryl/Main.java
@@ -0,0 +1,20 @@
+/*******************************************************
+ *  Name:Yevgeny Keyser, Patrick Lacey				   *
+ *  Asigment07: Project Euler Problem 13. 			   *																											
+ * 	Due Date: Tuesday, March 3, 2015			       *
+ * 	Description: Add large numbers					   *
+ * 	Course / Section:IT2045C Computer Programming 2	   *
+ *  Sources: Stack Overflow							   *
+ * 	Change History: None.							   *
+ *******************************************************/
+
+package edu.uc.IT2045C.Spring2015.Assignment07.keyseryl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
