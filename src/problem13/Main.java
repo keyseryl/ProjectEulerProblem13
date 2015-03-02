30db54048b175829c7b41345f6e81a8e580c7d54
patrick lacey <blacey182@gmail.com>
2015-03-02 13:37:48 -0500

    Added content to Main.java In the Package of: problem13

--- src/problem13/Main.java
+++ src/problem13/Main.java
@@ -1,9 +1,28 @@
 package problem13;
 
+import java.math.BigInteger;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//Calls the method: TestLargesum(); into the Main.
+		TestLargeSum();
+		//Prints the Answer of "LargeSum"
+		System.out.println("The answer is: " + TestLargeSum());
+	}
+	//Created the method: TestLargeSum 
+	public static Object TestLargeSum() {
+		/***
+		 * Calls the method "LargeSum" from the LargeSum Class
+		 * @return the LargeSum from: LargeSum.java
+		 */
+		//Declares LargeSum as Sum
+		  LargeSum Sum = new LargeSum();
+		 //BigInterger Answer is now equal to LargeSum
+		  BigInteger Answer = Sum.LargeSum();
+		  
+		 //Returns Answer
+			return Answer;
 
 	}
 
